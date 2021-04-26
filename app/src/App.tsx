7d49419dd54b567822c1bb6b9f9c8691640576e9
KornelStanulewicz <kornel.stanulewicz@gmail.com>
2021-04-26 18:24:17 +0200

    wykład2
    notatki z wykładu 2

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,37 +1,61 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from './views/home/Home';
+import Movie from './views/movie/Movie';
+import Page404 from './views/404page/Page404';
+
 import './App.css';
 
-import Counter from './components/Counter';
-import CustomHeader, { addNumber } from './components/CustomHeader';
-
-
 const App = () => {
-  const addedNumber = addNumber(1, 2);
-  const [changeNumber, setChangedNumber] = React.useState(0);
-
-  const handleNumberChange = ( newNumber: number ) => {
-    setChangedNumber(newNumber);
-  }
-
-  const liczbaMniejszaOdZera = (liczba: number) => {
-    if (liczba < 0){
-      return (<div>liczba jest mniejsza od 0</div>)
-    }
-  }
 
   return (
     <div className="App">
-      <CustomHeader>
-        {/* <Counter onNumberChange={(newNumber)=> { console.log(newNumber)}}/> */}
-        <Counter onNumberChange={handleNumberChange}/>
-        {
-          changeNumber > 0 && (<div> Liczba jest wieszka od 0</div>)
-        }
-        {liczbaMniejszaOdZera(changeNumber)}
-      </CustomHeader>   
-
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movie/:id" component={Movie} />
+          <Route path="/" component={Home} exact />
+          <Route path="*" component={Page404} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+// import Counter from './components/Counter';
+// import CustomHeader, { addNumber } from './components/CustomHeader';
+
+
+// const App = () => {
+//   const addedNumber = addNumber(1, 2);
+//   const [changeNumber, setChangedNumber] = React.useState(0);
+
+//   const handleNumberChange = ( newNumber: number ) => {
+//     setChangedNumber(newNumber);
+//   }
+
+//   const liczbaMniejszaOdZera = (liczba: number) => {
+//     if (liczba < 0){
+//       return (<div>liczba jest mniejsza od 0</div>)
+//     }
+//   }
+
+//   return (
+//     <div className="App">
+//       <CustomHeader>`
+//         {/* <Counter onNumberChange={(newNumber)=> { console.log(newNumber)}}/> */}
+//         <Counter onNumberChange={handleNumberChange}/>
+//         {
+//           changeNumber > 0 && (<div> Liczba jest wieszka od 0</div>)
+//         }
+//         {liczbaMniejszaOdZera(changeNumber)}
+//       </CustomHeader>   
+
+//     </div>
+//   );
+// }
+
+// export default App;
